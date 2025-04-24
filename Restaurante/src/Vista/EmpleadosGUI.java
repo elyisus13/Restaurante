3efcea389fa30f3c40a72c465c15f8587e75710f
elyisus13 <163328668+elyisus13@users.noreply.github.com>
2025-04-23 20:13:23 -0500

    Add files via upload

---
+++ Restaurante/src/Vista/EmpleadosGUI.java
@@ -0,0 +1,38 @@
+package Vista;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class EmpleadosGUI {
+    private JPanel panel1;
+    private JTable table1;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JButton agregarButton;
+    private JButton actualizarButton;
+    private JButton eliminarButton;
+
+    public EmpleadosGUI() {
+        agregarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        actualizarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        eliminarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+}
